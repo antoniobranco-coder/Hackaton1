fd209b0295acde08d0d481f8cc5de170b53bc433
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-04-29 14:01:11 +0100

    api test ok

--- hackaton1/src/components/Homepage.js
+++ hackaton1/src/components/Homepage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      getData: [],
+    };
   }
+
+  componentDidMount = () => {
+    const apiKey = `ILGosuG9SzGiLAQHiZtnbF91oKhuVi0W`;
+    const pathCountries = `list?show=countries`;
+    const pathCategories = `list?show=categories`;
+    const url = `https://api.windy.com/api/webcams/v2/${pathCountries}`;
+
+    axios
+      .get(url, { headers: { 'x-windy-key': apiKey } })
+      .then((response) => response.data)
+      .then((getData) => {
+        console.log(getData);
+      });
+  };
 
   render() {
     return (
