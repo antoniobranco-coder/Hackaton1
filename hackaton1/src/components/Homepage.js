ab0dbde3fc2d85a4daebc8c835dd73cbb14f5ddf
Antonio Branco <antoniobranco@sapo.pt>
2020-04-29 14:23:17 +0100

    first commit

--- hackaton1/src/components/Homepage.js
+++ hackaton1/src/components/Homepage.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 class Homepage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			getDataCategories: [],
+			getDataCountries: []
+		};
+	}
 
-  render() {
-    return (
-      <div className="Homepage">
-        <h1>Homepage</h1>
-      </div>
-    );
-  }
+	componentDidMount = () => {
+		const path = `list?show=countries`;
+		const url = `https://api.windy.com/api/webcams/v2/${path}`;
+
+		axios.get(url).then((response) => response.data).then((getDataCountries) => {
+			console.log(getDataCountries);
+		});
+	};
+
+	render() {
+		return (
+			<div className="Homepage">
+				<h1>Homepage</h1>
+				<Filters />
+			</div>
+		);
+	}
 }
 
 export default Homepage;
