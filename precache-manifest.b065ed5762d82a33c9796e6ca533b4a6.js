d0ca30a2765e1dbbe3182036738e84e340b70790
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-04-30 14:40:58 +0100

    Updates

---
+++ precache-manifest.b065ed5762d82a33c9796e6ca533b4a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69e3c7efa352007684de8787f87aaee",
+    "url": "/Hackaton1/index.html"
+  },
+  {
+    "revision": "a4789bf492e180dd78e9",
+    "url": "/Hackaton1/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "7e7eb600ced714887326",
+    "url": "/Hackaton1/static/css/main.38e69420.chunk.css"
+  },
+  {
+    "revision": "a4789bf492e180dd78e9",
+    "url": "/Hackaton1/static/js/2.e620e07e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Hackaton1/static/js/2.e620e07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7eb600ced714887326",
+    "url": "/Hackaton1/static/js/main.5d5ffd0c.chunk.js"
+  },
+  {
+    "revision": "07e46fb5c39a0bdaf0d6",
+    "url": "/Hackaton1/static/js/runtime-main.57b196f9.js"
+  }
+]);
